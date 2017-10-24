2dd36fadc29f307c304beb66ce3a3450886c297f
scotpfleghaar <scot.pfleghaar@gmail.com>
2017-10-24 15:18:52 -0500

    added the routing and exports
    There is now the routing in the controller that connects with the view to display the page.

--- server.js
+++ server.js
@@ -3,16 +3,29 @@ const express = require('express'); //adding a reference to express.js
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const http = require('http');
-const containter = require('/container');
+const container = require('/Users/scotpfleghaar/Documents/NodeChatApp/NodeChatApp/container.js');
 
-container.resolve(function () {
+container.resolve(function (users) {
     const app = SetupExpress();
 
     function SetupExpress() {
         const app = express();
         const server = http.createServer(app);
         server.listen(3000, function () {
-            console.log('listening on port 3000')
+            console.log('listening on port 3000');
         });
+        ConfigureExpress(app);
+        // Router
+        const router = require('express-promise-router')();
+        users.SetRouting(router);
+    }
+
+    function ConfigureExpress(app) {
+        app.use(express.static('public'));
+        app.set('view engine', 'ejs');
+        app.use(bodyParser.json());
+        app.use(bodyParser.urlencoded({
+            extended: true
+        }));
     }
 });
